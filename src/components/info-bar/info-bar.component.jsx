e9acb8565c9bd584b90bfbb337610faf388665a7
Vyash Bhawan <vyash@shogundev.com>
2021-01-15 14:38:29 +0100

    added services cards without functions

---
+++ src/components/info-bar/info-bar.component.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./info-bar.styles.scss";
+
+export default class InfoBar extends Component {
+  render() {
+    return <div className="info-bar">InfoBar</div>;
+  }
+}
---
+++ src/components/list-item/list-item.component.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import "./list-item.styles.scss";
+import felyxlogo from "../../assets/img/logos/logo_felyx.svg";
+import checklogo from "../../assets/img/logos/logo_check.svg";
+import gologo from "../../assets/img/logos/logo_go.png";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+
+export default class ServicesListItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      logo: {},
+    };
+  }
+
+  setCompany() {
+    const company = this.props.company;
+    switch (company) {
+      case "Felyx":
+        this.setState({ logo: felyxlogo });
+        break;
+      case "Check":
+        this.setState({ logo: checklogo });
+        break;
+      case "Go Share":
+        this.setState({ logo: gologo });
+        break;
+      default:
+        break;
+    }
+  }
+
+  componentDidMount() {
+    this.setCompany();
+  }
+  render() {
+    return (
+      <div className="list-item center">
+        <ListItem className="">
+          <ListItemAvatar>
+            <Avatar
+              className="list-item__avatar"
+              alt={this.props.company}
+              src={this.state.logo}
+            />
+          </ListItemAvatar>
+          <ListItemText
+            className="list-item__code"
+            primary={this.props.code}
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+            }}
+          />
+          <ListItemSecondaryAction>
+            {/* <Checkbox edge="end" /> */}
+            {/* <FileCopyIcon /> */}
+            <i className="far fa-clone"></i>
+          </ListItemSecondaryAction>
+        </ListItem>
+      </div>
+    );
+  }
+}
