7d451c4f77336a6bfe7504687c0c4bec4760ff32
Vyash Bhawan <vyash@shogundev.com>
2021-01-26 22:35:32 +0100

    added styling to infobar

--- src/components/info-bar/info-bar.component.jsx
+++ src/components/info-bar/info-bar.component.jsx
@@ -2,7 +2,6 @@ import React, { useContext, useEffect } from "react";
 import { Context } from "../../services/store";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
-import "../info-bar-item/info-bar-item.component";
 import "./info-bar.styles.scss";
 import "swiper/swiper.scss";
 import "swiper/components/navigation/navigation.scss";
@@ -13,29 +12,42 @@ SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 export default function InfoBar() {
   const { service, active } = useContext(Context);
   const [currentService, setCurrentService] = service;
+  const [currentActive, setCurrentActive] = active;
 
   // if (activeService === null) {
   //   return <
   // }
 
+  const setCompanyStyling = () => {
+    console.log(currentService.service_id);
+    switch (currentService.service_id) {
+      case "0":
+        return "info-bar info-bar__company--felyx";
+      case "1":
+        return "info-bar info-bar__company--check";
+      case "2":
+        return "info-bar info-bar__company--go  ";
+      default:
+        break;
+    }
+  };
+
   return (
-    <div className="info-bar">
-      <Swiper
-        className="info-bar__slider"
-        navigation
-        spaceBetween={50}
-        pagination={{ clickable: true }}
-        slidesPerView={1}
-      >
-        <SwiperSlide className="info-bar__slider__item">
-          {currentService.price}
-          {currentService.price_parking}
-        </SwiperSlide>
-        <SwiperSlide className="info-bar__slider__item">
-          {currentService.service_name}
-        </SwiperSlide>
-        <SwiperSlide className="info-bar__slider__item">Slide 4</SwiperSlide>
-      </Swiper>
+    <div className={setCompanyStyling()}>
+      <i class="fas fa-times close" onClick={() => setCurrentActive(null)}></i>
+      <div className="info-bar__slider__content">
+        <i class="fas fa-tag info-bar__slider__content--icon"></i>
+        <p className="info-bar__slider__content--price">
+          €{currentService.price}
+        </p>
+        <p style={{ fontSize: "0.5em" }}>per minuut</p>
+        <p
+          className="info-bar__slider__content--parking-price"
+          style={{ fontSize: "0.8em" }}
+        >
+          Parkeren: €{currentService.price_parking} per minuut
+        </p>
+      </div>
     </div>
   );
 }
