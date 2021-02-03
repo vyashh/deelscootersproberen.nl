eaab0b00805d2f9912a35766936898244d36df63
Vyash Bhawan <vyash@shogundev.com>
2021-02-03 13:29:55 +0100

    changed firebase import

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,6 @@
-import firebase from "firebase";
+import firebase from 'firebase/app';
 import "firebase/firestore";
+import 'firebase/analytics'
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
