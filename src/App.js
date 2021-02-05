4e48f2c8f2156416100e82fbbffa222769d20767
Vyash Bhawan <vyash@shogundev.com>
2021-02-05 21:57:08 +0100

    added sitemap.xml to public folder

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect 
-} from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
 import "./App.scss";
 import Store from "./services/store";
 import HomePage from "./pages/homepage";
 
 export default function App() {
   return (
-    <Router>
-    <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route render={() => <Redirect to={{pathname: "/"}} />} />
-    </Switch>
-    </Router>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        // random
+        <Route render={() => <Redirect to={{ pathname: "/" }} />} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
-
-function Home(){
+function Home() {
   return (
-      <Store>
-         <HomePage />
-      </Store>
-  )
+    <Store>
+      <HomePage />
+    </Store>
+  );
 }
