398e6d2f423a0658c88309d8c37fd2625f4dd466
Vyash Bhawan <vyash@shogundev.com>
2021-02-05 16:17:19 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect 
+} from "react-router-dom";
 import "./App.scss";
 import Store from "./services/store";
 import HomePage from "./pages/homepage";
 
 export default function App() {
   return (
-    <Store>
-      <HomePage />
-    </Store>
+    <Router>
+    <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route render={() => <Redirect to={{pathname: "/"}} />} />
+    </Switch>
+    </Router>
   );
+}
+
+
+function Home(){
+  return (
+      <Store>
+         <HomePage />
+      </Store>
+  )
 }
